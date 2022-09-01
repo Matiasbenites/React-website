2cb6ddf81a05bd5529d629e63247ae35aa5d7a11
Matias Benites <matiasbenites665@gmail.com>
2022-09-01 19:47:22 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <Row>
+
+   <h1>HOLA!</h1>
+    </Row>
+  </div>;
+}
+
+export default App;
