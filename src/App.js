28378dbc5cee54560fe438bec4f14022bd0c8428
Matias Benites <matiasbenites665@gmail.com>
2023-01-22 20:32:59 -0300

    feat:Project finished

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import SignUp from "./components/pages/SignUp";
 import Products from "./components/pages/Products";
 import Footer from "./components/Footer/Footer";
-=======
-import Home from "./components/Home/Home";
->>>>>>> refs/remotes/origin/main
 
 function App() {
   return (
@@ -18,20 +14,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-<<<<<<< HEAD
           <Route path="/" exact element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
         <Footer />
-=======
-          <Route path="/" exact component={Home} />
-          {/* <Route path="/services" component={Services} />
-          <Route path="/products" component={Products} />
-          <Route path="/sign-up" component={SignUp} /> */}
-        </Routes>
->>>>>>> refs/remotes/origin/main
       </Router>
     </>
   );
